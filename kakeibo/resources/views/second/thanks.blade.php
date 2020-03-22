faa5e089a43dd751b750252e7f127b8045b40575
yosuke <cy04kumakuma.mac@gmail.com>
2020-03-22 10:57:14 +0900

    thanks.phpからloginへのroute修正

--- kakeibo/resources/views/second/thanks.blade.php
+++ kakeibo/resources/views/second/thanks.blade.php
@@ -6,7 +6,7 @@
     <div class="thanks_body">
       <div>
         <p>登録が完了しました</p>
-        <p><a href="/login">ログインする</a></p>
+        <p><a href="/">ログインする</a></p>
       </div>
     </div>
 @endsection
